c1c805584f1ac4853b43ee2d5cc5cd7a385841a0
Aidan Halim <151803107+AidanHalim@users.noreply.github.com>
2025-03-25 15:39:45 +1100

    connected staff with subject, and student with submission

--- app/src/main/java/com/unimelb/swen30006/workshops/Staff.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Staff.java
@@ -1,20 +1,51 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Staff extends Person {
     String[] roles;
-    String[] Staffid;
+    String[] staffID;
+    ArrayList<Subject> subjects;
 
-    public Staff(String[] address, Date dob, String[] name, String[] roles, String[] Staffid) {
+    public Staff(String[] address, Date dob, String[] name, String[] roles, String[] staffID) {
         this.address = address;
         this.dob = dob;
         this.name = name;
-        this.Staffid = roles;
-        this.roles = Staffid;
+        this.roles = roles;
+        this.staffID = staffID;
+        this.subjects = new ArrayList<>();
     }
 
+    public void addSubject(Subject subject){
+        subjects.add(subject);
+    }
 
+    public void removeSubject(Subject subject){
+        subjects.remove(subject);
+    }
 
+    public String[] getRoles() {
+        return roles;
+    }
 
+    public void setRoles(String[] roles) {
+        this.roles = roles;
+    }
+
+    public String[] getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String[] staffID) {
+        this.staffID = staffID;
+    }
+
+    public ArrayList<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(ArrayList<Subject> subjects) {
+        this.subjects = subjects;
+    }
 }
--- app/src/main/java/com/unimelb/swen30006/workshops/Student.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -1,10 +1,12 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Student extends Person {
     String[] email;
     String[] studentID;
+    ArrayList<Submission> submissions;
 
     public Student(String[] address, Date dob, String[] name, String[] email, String[] studentID) {
         this.address = address;
@@ -12,6 +14,7 @@ public class Student extends Person {
         this.name = name;
         this.studentID = studentID;
         this.email = email;
+        this.submissions = new ArrayList<>();
     }
 
     private float getTotalGrade(float totalgrade){
@@ -22,4 +25,31 @@ public class Student extends Person {
 
     }
 
+    private void submitSubmission(Submission submission){
+        this.submissions.add(submission);
+    }
+
+    public String[] getEmail() {
+        return email;
+    }
+
+    public void setEmail(String[] email) {
+        this.email = email;
+    }
+
+    public String[] getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String[] studentID) {
+        this.studentID = studentID;
+    }
+
+    public ArrayList<Submission> getSubmissions() {
+        return submissions;
+    }
+
+    public void setSubmissions(ArrayList<Submission> submissions) {
+        this.submissions = submissions;
+    }
 }
